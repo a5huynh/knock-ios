4b62047e74c6e923a5dec1b7365c6901504bb287
Andrew Huynh <a5.t.huynh@gmail.com>
2021-04-19 11:43:51 -0700

    Initial Commit

---
+++ Shared/knock_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  knock_iosApp.swift
+//  Shared
+//
+//  Created by Andrew Huynh on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct knock_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
